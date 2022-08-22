533603649967421a39af62d73cee34c9ef1b1325
Toni Peraira <toniperairam@gmail.com>
2022-08-22 22:06:52 +0200

    Add Counters with React useState
    Add `CountersReact.jsx` component that implements `useState` from React

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-function App() {
+import CountersReact from './components/CountersReact'
+
+const App = () => {
   return (
     <div className="App">
       <h1>React Hooks: useState Demo</h1>
+      <CountersReact />
     </div>
   )
 }
